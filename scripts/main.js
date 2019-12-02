49fddced2f36dd80c301c844281153c1ad9e8e46
UVic AI Club <57778277+uvicaiclub@users.noreply.github.com>
2019-12-01 22:22:47 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+/*
+test.js 
+for the UVic AI Club website.
+Created by Debrah Wyatt
+Nov 29, 2019
+*/
+
+//Hello World
+//window.alert("HERE");
+
+//Won't show content until whole file is available
+function unveil() {
+  var thebod = document.getElementById("testbody");
+  thebod.setAttribute('style', "display: block");
+}
+
---
+++ test/test.js
@@ -0,0 +1,24 @@
+/*
+test.js 
+for the UVic AI Club website.
+Created by Debrah Wyatt
+Nov 29, 2019
+*/
+
+//Creates a new http request for XML document
+var txtHttp = new XMLHttpRequest();
+txtHttp.onreadystatechange = function() { 
+        if (txtHttp.readyState == 4 && txtHttp.status == 200)
+            callback(txtHttp.responseText);
+    }
+
+txtHttp.open("GET", 'https://uvicaiclub.ca/test/test.xml', true); // true for asynchronous 
+//BUG - I dunno why this is here
+txtHttp.send(null);
+
+
+//Prints the object returned
+console.log(txtHttp);
+
+//prints the responseText
+console.log(txtHttp.responseText);
